4b9a9417e6e21c8ccbf42602d3074322ef8b3bd8
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-10-27 21:07:56 +0100

    aaa

--- src/utils/database/database_connection.php
+++ src/utils/database/database_connection.php
@@ -6,6 +6,7 @@
         $db_user = "root";
         $db_password = "00000000";
         $conn = mysqli_connect($db_server,$db_user,$db_password,$db_name);
+        mysqli_store_result($conn);
         if($conn) {
             return $conn;
         }
