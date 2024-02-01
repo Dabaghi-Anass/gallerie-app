f2fb1b4f08b2f3f5f3d7da51ad2a7b6ee3879538
anassdabaghi <anass.debbaghi123@gmail.com>
2024-02-01 18:54:41 +0100

    Update database_connection.php

--- src/utils/database/database_connection.php
+++ src/utils/database/database_connection.php
@@ -1,10 +1,10 @@
 <?php 
     $conn = "";
     function connectToDb() {
-        $db_name = "gallerie_app";
-        $db_server = "localhost" ;
-        $db_user = "root";
-        $db_password = "00000000";
+        $db_name = "b9fvyugluxs9htc4kspb";
+        $db_server = "b9fvyugluxs9htc4kspb-mysql.services.clever-cloud.com" ;
+        $db_user = "ud7uulszn0adb3bk";
+        $db_password = "HVut2aiwJmiccyXjz5jB";
         $conn = mysqli_connect($db_server,$db_user,$db_password,$db_name);
         mysqli_store_result($conn);
         if($conn) {
